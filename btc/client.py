f7f07bbb38bde1dfc3d6a11e40beb3274566a82e
maxidrum <maxidrum@mail.ru>
2018-01-25 01:20:03 +0200

    update structure

---
+++ btc/client.py
@@ -0,0 +1,48 @@
+import requests
+
+
+BASE_URL = "https://btc-trade.com.ua/api/"
+
+
+class Client(object):
+
+    def get_deals(self):
+        pass
+
+    def get_trades_sell(self):
+        pass
+
+    def get_trades_buy(self):
+        pass
+
+    def get_japan_stat(self):
+        pass
+
+    #Privat api
+    def auth_request(self):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def bid_sell(self):
+        pass
+
+    def bid_buy(self):
+        pass
+
+    def my_orders(self):
+        pass
+
+    def order_status(self):
+        pass
+
+    def remove_order(self):
+        pass
+
+    def ask_coin(self):
+        pass
+
+    def bid_coin(self):
+        pass
+
